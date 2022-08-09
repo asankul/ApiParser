79bf7515803a5ad6e73d49596cdaea269d839485
asankul <asankul1993@gmail.com>
2022-08-09 16:06:41 +0300

    Initial Commit

---
+++ ApiParser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiParser
+//
+//  Created by Асанкул Садыков on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
